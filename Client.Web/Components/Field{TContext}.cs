854030b0b45d197ac5e212256e66a94ac15caf1f
evanderzwan <evanderzwan@gmail.com>
2025-07-01 20:42:07 +0200

    update

--- Client.Web/Components/Field{TContext}.cs
+++ Client.Web/Components/Field{TContext}.cs
@@ -4,12 +4,12 @@ namespace Foundation.Components;
 
 public abstract class Field<TContext> : ComponentBase, IDisposable where TContext : class
 {
+    [Parameter(CaptureUnmatchedValues = true)] public IReadOnlyDictionary<string, object?>? Attributes { get; set; }
+
     [CascadingParameter] protected TContext? Context { get; private set; }
     [CascadingParameter] protected Form<TContext>? Form { get; private init; }
 
-    [Parameter(CaptureUnmatchedValues = true)] public IReadOnlyDictionary<string, object?>? Attributes { get; set; }
     [Parameter] public bool AutoFocus { get; set; }
-    [Parameter] public bool Disabled { get; set; }
     [Parameter] public string? Title { get; set; }
     [Parameter] public bool Visible { get; set; } = true;
 
