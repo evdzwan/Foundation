13e9b5fa310ddd50d2d7db8274cb013c7adb6f02
evanderzwan <evanderzwan@gmail.com>
2025-07-03 20:55:41 +0200

    update

--- Client.Web/Scripting/FoundationConfigurationExtensions.cs
+++ Client.Web/Scripting/FoundationConfigurationExtensions.cs
@@ -5,14 +5,12 @@ namespace Foundation.Scripting;
 
 public static class FoundationConfigurationExtensions
 {
-    public static IFoundationConfiguration AddScripts(this IFoundationConfiguration @this, IEnumerable<Assembly> assemblies)
-        => @this.AddScripts(assemblies.Prepend(typeof(FoundationConfigurationExtensions).Assembly)
-                                      .SelectMany(asm => asm.GetTypes()));
-
-    public static IFoundationConfiguration AddScripts(this IFoundationConfiguration @this, IEnumerable<Type> types)
+    public static IFoundationConfiguration AddScripts(this IFoundationConfiguration @this, IEnumerable<Assembly>? assemblies = null)
     {
-        types.Where(type => !type.IsAbstract && type.IsAssignableTo(typeof(Script)))
-             .ForEach(scriptType => @this.Services.AddTransient(scriptType));
+        (assemblies ?? []).Prepend(typeof(FoundationConfigurationExtensions).Assembly)
+                          .SelectMany(asm => asm.GetTypes())
+                          .Where(type => !type.IsAbstract && type.IsAssignableTo(typeof(Script)))
+                          .ForEach(scriptType => @this.Services.AddTransient(scriptType));
 
         return @this;
     }
