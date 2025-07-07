65be1571ec010572be48f42848d43a8714e0b709
evanderzwan <evanderzwan@gmail.com>
2025-07-07 12:31:42 +0200

    update

--- Client/AsyncCollection.cs
+++ Client/AsyncCollection.cs
@@ -6,18 +6,33 @@ namespace Foundation;
 public static class AsyncCollection
 {
     public static IAsyncCollection<TItem> Create<TItem>(Func<Query, CancellationToken, Task<TItem[]>> resolve) where TItem : notnull
-        => new RelayAsyncCollection<TItem>(resolve);
+        => new RelayAsyncCollection<TItem>(viewSize: 20, resolve); //TODO anders oplossen
 
-    sealed class RelayAsyncCollection<TItem>(Func<Query, CancellationToken, Task<TItem[]>> resolve) : IAsyncCollection<TItem> where TItem : notnull
+    sealed class RelayAsyncCollection<TItem>(int viewSize, Func<Query, CancellationToken, Task<TItem[]>> resolve) : IAsyncCollection<TItem> where TItem : notnull
     {
-        readonly ConcurrentDictionary<Query, Task<TItem[]>> Views = [];
+        readonly ConcurrentDictionary<int, Task<TItem[]>> Views = [];
+
+        public bool Complete { get; private set; }
+
+        public int Count
+        {
+            get
+            {
+                if (Views.IsEmpty)
+                {
+                    return 0;
+                }
+
+                var maxWindowKey = Views.Keys.Max();
+                return maxWindowKey * viewSize + Views[maxWindowKey].Result.Length + (Complete ? 0 : viewSize);
+            }
+        }
 
         public async IAsyncEnumerator<TItem> GetAsyncEnumerator(CancellationToken cancellationToken = default)
         {
-            var query = new Query(Page: new(Skip: 0, Take: 20));
             for (var i = 0; ; i++)
             {
-                var items = await GetView(query with { Page = query.Page! with { Skip = i * query.Page.Take } }, cancellationToken);
+                var items = await GetView(new(Page: new(Skip: i * viewSize, Take: viewSize)), cancellationToken);
                 if (items.Length == 0)
                 {
                     yield break;
@@ -27,19 +42,40 @@ public static class AsyncCollection
                 {
                     yield return item;
                 }
-
-                //TODO anders oplossen
-                if (items.Length < query.Page.Take)
-                {
-                    yield break;
-                }
             }
         }
 
         public Task<TItem[]> GetValue(CancellationToken cancellationToken = default)
             => this.ToArray(cancellationToken);
 
-        public Task<TItem[]> GetView(Query query, CancellationToken cancellationToken = default)
-            => Views.GetOrAdd(query, t => resolve(query, cancellationToken));
+        public async Task<TItem[]> GetView(Query query, CancellationToken cancellationToken = default)
+        {
+            var skip = query.Page?.Skip ?? 0;
+            var take = query.Page?.Take ?? viewSize;
+
+            var windowStart = (int)Math.Floor(skip / (viewSize * 1d));
+            var windowCount = (int)Math.Ceiling(take / (viewSize * 1d));
+
+            var items = new List<TItem>();
+            foreach (var i in Enumerable.Range(windowStart, windowCount))
+            {
+                var windowItems = await Views.GetOrAdd(i, j => resolve(query with { Page = new(Skip: j * viewSize, Take: viewSize) }, cancellationToken));
+                items.AddRange(windowItems);
+
+                if (windowItems.Length < viewSize)
+                {
+                    Complete = true;
+                    break;
+                }
+            }
+
+            return [.. items.Skip(skip - windowStart * viewSize).Take(take)];
+        }
+
+        public void Reset()
+        {
+            Complete = false;
+            Views.Clear();
+        }
     }
 }
--- Client/IAsyncCollection{TItem}.cs
+++ Client/IAsyncCollection{TItem}.cs
@@ -4,5 +4,10 @@ namespace Foundation;
 
 public interface IAsyncCollection<TItem> : IAsyncEnumerable<TItem>, IAsyncValue<TItem[]> where TItem : notnull
 {
+    bool Complete { get; }
+    int Count { get; }
+
     Task<TItem[]> GetView(Query query, CancellationToken cancellationToken = default);
+    void Reset();
 }
+
