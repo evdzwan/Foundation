d265832814641be6d718b3226b4b5cbbb0cfb759
evanderzwan <evanderzwan@gmail.com>
2025-07-07 13:53:48 +0200

    update

---
+++ Client.Web/Components/Respond.razor.js
@@ -0,0 +1,18 @@
+﻿export function attach(elem, invoker) {
+    if (elem !== null) {
+        elem.resize = () => resize(elem, invoker);
+        window.addEventListener("resize", elem.resize);
+        resize(elem, invoker);
+    }
+}
+
+export function detach(elem) {
+    if (elem !== null) {
+        window.removeEventListener("resize", elem.resize);
+        elem.resize = undefined;
+    }
+}
+
+async function resize(elem, invoker) {
+    await invoker.invokeMethodAsync("UpdateBounds", elem.parentElement.clientWidth, elem.parentElement.clientHeight);
+}
---
+++ Client.Web/Scripts/RespondScript.cs
@@ -0,0 +1,12 @@
+﻿using Foundation.Scripting;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace Foundation.Scripts;
+
+[Script("./_content/Client.Web/Components/Respond.razor.js")]
+sealed partial class RespondScript
+{
+    public partial ValueTask Attach<TValue>(ElementReference elem, DotNetObjectReference<TValue> invoker) where TValue : class;
+    public partial ValueTask Detach(ElementReference elem);
+}
