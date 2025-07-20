3505ed38aa8a9291fc50ca8d375edd0f923a56d9
evanderzwan <evanderzwan@gmail.com>
2025-07-20 20:19:11 +0200

    added disposable type

---
+++ Foundation/Disposable.cs
@@ -0,0 +1,9 @@
+﻿namespace Foundation;
+
+public sealed class Disposable(Action action) : IDisposable
+{
+    public static Disposable Empty { get; } = new(() => { });
+
+    void IDisposable.Dispose()
+        => action();
+}
