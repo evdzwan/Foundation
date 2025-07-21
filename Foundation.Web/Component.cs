361e31e9d6338ca3234cf6d5a5f895053dfa5a1e
evanderzwan <evanderzwan@gmail.com>
2025-07-21 12:00:55 +0200

    added component base type

---
+++ Foundation.Web/Component.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Foundation;
+
+public abstract class Component : ComponentBase, IAsyncDisposable
+{
+    readonly CancellationTokenSource CancellationTokenSource = new();
+
+    protected virtual void OnDisposing() { }
+    protected virtual Task OnDisposingAsync() => Task.CompletedTask;
+    async ValueTask IAsyncDisposable.DisposeAsync()
+    {
+        OnDisposing();
+        await OnDisposingAsync();
+
+        await CancellationTokenSource.CancelAsync();
+        CancellationTokenSource.Dispose();
+        GC.SuppressFinalize(this);
+    }
+}
