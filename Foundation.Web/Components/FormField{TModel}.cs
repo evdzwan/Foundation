fe9b498e9d80a8be514b15368040211d1202d12e
evanderzwan <evanderzwan@gmail.com>
2025-07-21 12:14:45 +0200

    added formfield base type

---
+++ Foundation.Web/Components/FormField{TModel}.cs
@@ -0,0 +1,46 @@
+﻿using Foundation.Expressions;
+using Humanizer;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Foundation.Components;
+
+public abstract class FormField<TModel> : Component
+{
+    [CascadingParameter] Form<TModel>? Form { get; set; }
+    [CascadingParameter] EditContext? FormContext { get; set; }
+    [Parameter, EditorRequired] public required Expression<Func<TModel, object?>> Expression { get; set; }
+    [Parameter] public string? Title { get; set; }
+
+    protected object? Value
+    {
+        get
+        {
+            if (ValueProperty is { CanRead: true } property && FormContext?.Model is TModel { } model)
+            {
+                return property.GetValue(model);
+            }
+
+            return default;
+        }
+        set
+        {
+            if (ValueProperty is { CanWrite: true } property && FormContext?.Model is TModel { } model)
+            {
+                property.SetValue(model, value);
+            }
+        }
+    }
+
+    protected Expression<Func<object?>>? ValueExpression { get; private set; }
+    protected PropertyInfo? ValueProperty { get; private set; }
+
+    protected override void OnParametersSet()
+    {
+        ValueExpression ??= FormContext?.Model is TModel { } model ? Expression.ReplaceParameter(model) : null;
+        ValueProperty ??= ValueExpression?.GetPropertyOrDefault();
+        Title ??= ValueProperty?.Name.Humanize(LetterCasing.Title);
+    }
+}
