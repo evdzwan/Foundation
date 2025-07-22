78bfffae15173484c8a908456bfe5a5ffab00154
evanderzwan <evanderzwan@gmail.com>
2025-07-22 11:50:42 +0200

    form demo

---
+++ Foundation.Demo/Protocol/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace Foundation.Protocol;
+
+public sealed record Book(int Id)
+{
+    public required string Title { get; set; }
+    public required string Summary { get; set; }
+    public required DateOnly PublicationDate { get; set; }
+    public required int AuthorId { get; set; }
+}
---
+++ Foundation.Demo/Repositories/BookRepository.cs
@@ -0,0 +1,17 @@
+﻿using Foundation.Protocol;
+
+namespace Foundation.Repositories;
+
+public static class BookRepository
+{
+    public static Book Create(int id) => new(id)
+    {
+        Title = $"Title of Book {id}",
+        Summary = $"Summary of...\r\nBook {id}",
+        PublicationDate = DateOnly.FromDateTime(DateTime.Today).AddDays(-id),
+        AuthorId = (id - 1) % 1_000 + 1
+    };
+
+    public static Book CreateRandom()
+        => Create(Random.Shared.Next(1, 10_000));
+}
