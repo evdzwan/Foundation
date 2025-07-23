0953866a2fff99910ce0419cc4601c040df1dd5c
evanderzwan <evanderzwan@gmail.com>
2025-07-23 12:56:45 +0200

    choice field improvements

--- Foundation.Web/Scripts/ChoiceFieldScript.cs
+++ Foundation.Web/Scripts/ChoiceFieldScript.cs
@@ -4,7 +4,7 @@ using Microsoft.JSInterop;
 
 namespace Foundation.Scripts;
 
-[Script(@"./_content/Client.Web/Components/ChoiceField.razor.js")]
+[Script(@"./_content/Foundation.Web/Components/ChoiceField.razor.js")]
 sealed partial class ChoiceFieldScript
 {
     public partial ValueTask Attach<TValue>(ElementReference elem, DotNetObjectReference<TValue> invoker, CancellationToken cancellationToken = default) where TValue : class;
