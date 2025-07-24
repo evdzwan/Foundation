bec24f12157fa983c64d81446ca3b79d708954a9
evanderzwan <evanderzwan@gmail.com>
2025-07-24 19:27:49 +0200

    added menu

---
+++ Foundation.Web/Scripts/MenuScript.cs
@@ -0,0 +1,14 @@
+﻿using Foundation.Scripting;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace Foundation.Scripts;
+
+[Script(@"./_content/Foundation.Web/Components/Menu.razor.js")]
+sealed partial class MenuScript
+{
+    public partial ValueTask Attach<TValue>(ElementReference elem, DotNetObjectReference<TValue> invoker, CancellationToken cancellationToken = default) where TValue : class;
+    public partial ValueTask Detach(ElementReference elem, CancellationToken cancellationToken = default);
+    public partial ValueTask ShowPopover(ElementReference elem, ElementReference? trigger, CancellationToken cancellationToken = default);
+    public partial ValueTask HidePopover(ElementReference elem, CancellationToken cancellationToken = default);
+}
