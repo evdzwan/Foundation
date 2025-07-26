acf17ec29ffeaf31fe6873705690cf9b116140f5
evanderzwan <evanderzwan@gmail.com>
2025-07-26 21:57:54 +0200

    added respond component

---
+++ Foundation.Web/Scripts/RespondScript.cs
@@ -0,0 +1,12 @@
+﻿using Foundation.Scripting;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace Foundation.Scripts;
+
+[Script("./_content/Foundation.Web/Components/Respond.razor.js")]
+sealed partial class RespondScript
+{
+    public partial ValueTask Attach<TValue>(ElementReference elem, DotNetObjectReference<TValue> invoker, CancellationToken cancellationToken = default) where TValue : class;
+    public partial ValueTask Detach(ElementReference elem, CancellationToken cancellationToken = default);
+}
