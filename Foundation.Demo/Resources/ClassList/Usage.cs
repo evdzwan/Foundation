5e3c3d619b96123abb0c9af8d7ac2c201fb8c4ff
evanderzwan <evanderzwan@gmail.com>
2025-07-27 13:28:54 +0200

    added classlist demo

---
+++ Foundation.Demo/Resources/ClassList/Usage.cs
@@ -0,0 +1,15 @@
+﻿// create a class list by the empty ctor or like this
+var list = new ClassList("my-class");
+
+// add classes like this; the last parameter is true by default
+list.Add("disabled", IsDisabled);
+
+// or use the additional operators
+list += "active";
+
+// class list is commonly used like this
+public string CssClass => new ClassList().Add("my-class")
+                                         .Add("active", IsActive)
+                                         .Add("disabled", IsDisabled);
+
+// the resulting class for a non-active, disabled state would be 'my-class disabled'
