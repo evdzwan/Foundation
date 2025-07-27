b1ad1932c7988073e6412e1a5960b675463c2a7b
evanderzwan <evanderzwan@gmail.com>
2025-07-27 14:48:50 +0200

    added asyncvalue demo

---
+++ Foundation.Demo/Resources/AsyncValue/Usage.cs
@@ -0,0 +1,16 @@
+﻿// create an asyncvalue like this; even though the actual value hasn't been retrieved yet, the type can be assigned to a readonly variable
+readonly IAsyncValue<string> Value = AsyncValue.Create(async () =>
+{
+    // retrieve your data asynchronously
+    return await MyRepository.Get(MyId);
+});
+
+// retrieve actual value like this
+var str = await Value.GetValue();
+
+// value is cached, so this call won't do anything and just return the value
+var str2 = await Value.GetValue();
+
+// you can reset the cache so the delegate will be called again
+Value.Reset();
+var str3 = await Value.GetValue();
