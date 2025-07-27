005f7f63f5b3096969684bece8c7a2b6d772c10d
evanderzwan <evanderzwan@gmail.com>
2025-07-27 14:49:24 +0200

    added selection demo

---
+++ Foundation.Demo/Resources/Selection/Usage.cs
@@ -0,0 +1,19 @@
+﻿// create a selection like this
+readonly ISelection<string> SingleSelection = Selection.Single<string>();
+readonly ISelection<string> MultipleSelection = Selection.Multiple<string>();
+
+// or use one of the other overloads
+var singleWithDefault = Selection.Single("initial-selection");
+var multipleWithDefault = Selection.Multiple(new[] { "initial", "selection" });
+
+// selection is an ilist, so you can use normal list operations
+var secondSelectedItem = MultipleSelection[1];
+
+// get the last selected or active item with the cursor property
+var activeItem = MultipleSelection.Cursor;
+
+// activate, deactivate or toggle an item
+MultipleSelection.Activate("my-value"); // selection now contains "my-value"
+MultipleSelection.Deactivate("my-value"); // selection doesn't contain "my-value"
+MultipleSelection.Toggle("my-value"); // selection now contains "my-value" again
+MultipleSelection.Toggle("my-value"); // selection doesn't contain "my-value"
