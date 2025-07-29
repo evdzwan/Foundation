de6134c69f03692756813a516c4da59aa12bd5cf
evanderzwan <evanderzwan@gmail.com>
2025-07-29 14:04:08 +0200

    sum extension for inumber<>

---
+++ Foundation/Collections/NumberExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Numerics;
+
+namespace Foundation.Collections;
+
+public static class NumberExtensions
+{
+    public static INumber<TValue> Sum<TValue>(this IEnumerable<TValue> @this) where TValue : INumber<TValue>
+    {
+        var sum = TValue.Zero;
+        @this.ForEach(value => sum += value);
+        return sum;
+    }
+}
