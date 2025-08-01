0f17d01637105485d588aba11b6bbdc45cdc949f
Erik van der Zwan <E.van.der.Zwan@chipsoft.com>
2025-08-01 15:26:22 +0200

    added script extensions

--- Foundation.Web/ServiceCollectionExtensions.cs
+++ Foundation.Web/ServiceCollectionExtensions.cs
@@ -11,12 +11,18 @@ public static class ServiceCollectionExtensions
         => @this.AddScripts([typeof(ServiceCollectionExtensions).Assembly])
                 .AddScoped<MenuController>();
 
+    public static IServiceCollection AddScript<TScript>(this IServiceCollection @this) where TScript : Script
+        => @this.AddScript(typeof(TScript));
+
+    public static IServiceCollection AddScript(this IServiceCollection @this, Type scriptType)
+        => @this.AddTransient(scriptType);
+
     public static IServiceCollection AddScripts(this IServiceCollection @this, IEnumerable<Assembly> assemblies)
     {
         assemblies.SelectMany(asm => asm.GetTypes())
                   .Where(type => type is { IsAbstract: false, IsClass: true, IsGenericType: false })
                   .Where(type => type.IsAssignableTo(typeof(Script)))
-                  .ForEach(scriptType => @this.AddTransient(scriptType));
+                  .ForEach(scriptType => @this.AddScript(scriptType));
 
         return @this;
     }
