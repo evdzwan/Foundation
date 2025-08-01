3c48e2813596011408cee345a90e0e33cfb8d63c
Erik van der Zwan <E.van.der.Zwan@chipsoft.com>
2025-08-01 16:31:44 +0200

    added icanvasscript

---
+++ Foundation.Web/Scripting/ICanvasScript.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace Foundation.Scripting;
+
+public interface ICanvasScript
+{
+    ValueTask Cleanup(ElementReference canvas, CancellationToken cancellationToken = default);
+    ValueTask Initialize<TValue>(ElementReference canvas, DotNetObjectReference<TValue> invoker, CancellationToken cancellationToken = default) where TValue : class;
+    ValueTask Resize(ElementReference canvas, CancellationToken cancellationToken = default);
+}
