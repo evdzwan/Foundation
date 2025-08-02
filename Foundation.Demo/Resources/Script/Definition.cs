1a1d33c23c55b3e8cfa6e061e8f988adcd39a3e7
evanderzwan <evanderzwan@gmail.com>
2025-08-02 21:59:17 +0200

    script demo

--- Foundation.Demo/Resources/Script/Definition.cs
+++ Foundation.Demo/Resources/Script/Definition.cs
@@ -1,1 +1,6 @@
-﻿// TBD
+﻿[Script("./_content/My.Project/Components/MyComponent.razor.js")]
+sealed partial class MyComponentScript
+{
+    public partial ValueTask Attach<TValue>(DotNetObjectReference<TValue> component, ElementReference element, CancellationToken cancellationToken = default) where TValue : class;
+    public partial ValueTask Detach<TValue>(DotNetObjectReference<TValue> component, CancellationToken cancellationToken = default) where TValue : class;
+}
--- Foundation.Demo/Resources/Script/Registration.cs
+++ Foundation.Demo/Resources/Script/Registration.cs
@@ -1,1 +1,8 @@
-﻿// TBD
+﻿// Foundation's own scripts are loaded on buider.Services.AddFoundation()
+builder.Services.AddFoundation();
+
+// Add your own script like this
+builder.Services.AddScript<MyComponentScript>();
+
+// Or add all scripts from specific assemblies like this
+builder.Services.AddScripts([typeof(MyComponentScript).Assembly, typeof(OtherScript).Assembly]);
